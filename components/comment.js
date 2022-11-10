2f14450b5f4e5ca7c9f7eb5ff2d6c5f82363133c
Malik Piara <malikpiara@gmail.com>
2022-11-10 23:02:00 +0100

    Displaying an avatar for every author in the comments.

--- components/comment.js
+++ components/comment.js
@@ -1,16 +1,20 @@
 import { parseISO, format } from 'date-fns';
 import commentStyles from './comment.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Comment({comment}) {
   // const date = parseISO(dateString);
   return (
     <>
-    <div className={commentStyles.commentContainer}>
+    <div className={`${commentStyles.commentContainer} ${utilStyles.flex} ${utilStyles.gap20}`}>
+      <div className={commentStyles.avatar}>{comment.author.slice(0, 1)}</div>
+      <div>
         <div className={commentStyles.commentHead}>
             <>{comment.author}</>
         </div>
         <div className={commentStyles.commentBody}>
             <p>{comment.content}</p>
+        </div>
         </div>
     </div>
     </>
