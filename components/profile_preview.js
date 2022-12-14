9258e5f38a43ad8b9132391021326bd466f0ed36
Malik Piara <malikpiara@gmail.com>
2022-12-14 17:38:25 +0100

    Started working on profile preview component that shows user information when profile is hovered.

---
+++ components/profile_preview.js
@@ -0,0 +1,22 @@
+import commentStyles from './comment.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+export default function ProfilePreview() {
+  return (
+    <>
+    <div className={utilStyles.profilePreviewWrapper}>
+          <div className={`${commentStyles.commentContainer} ${utilStyles.flex} ${utilStyles.gap20}`}>
+            <div className={`${utilStyles.avatarPlaceholder} ${utilStyles.pulse}`}/>
+            <div>
+              <div className={commentStyles.commentHead}>
+                <div className={`${utilStyles.commentAuthorPlaceholder} ${utilStyles.pulse}`}></div>
+              </div>
+              <div className={commentStyles.commentBody}>
+                <div className={`${utilStyles.commentPlaceholder} ${utilStyles.pulse}`}></div>
+              </div>
+            </div>
+          </div>
+    </div>
+      </>
+  );
+}
