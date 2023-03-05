b353b8807cd490fc72fff6be9c5349c74020b378
Malik Piara <malikpiara@gmail.com>
2023-03-05 02:06:12 +0100

    Added analytics link to dashboard. Minor styling changes.

--- components/adminNav.js
+++ components/adminNav.js
@@ -3,8 +3,9 @@ import SpeechBubble from './icons/speechIcon.js';
 import Heart from './icons/heartIcon.js';
 import PostsIcon from './icons/postsIcon.js';
 import Link from 'next/link';
+import Image from 'next/image';
+import miniLogo from '../public/favicon.png';
 import { useRouter } from 'next/router';
-import GateIcon from './icons/worldIcon';
 import WorldIcon from './icons/worldIcon';
 
 export default function AdminNav({onClick}) {
@@ -29,13 +30,17 @@ export default function AdminNav({onClick}) {
                         <Heart/>
                         <Link href="/admin/subscribers">Subscribers</Link>
                     </div>
-                    <div onClick={onClick} className={getNavItemClassName(router, "/admin/post")} >
-                        <PostsIcon/>
-                        <Link href="#">See Posts</Link>
-                    </div>
                     <div className={getNavItemClassName(router, "/admin/linkpage")} >
                         <WorldIcon/>
                         <Link href="/admin/linkpage">LinkPage</Link>
+                    </div>
+                    <div onClick={onClick} className={getNavItemClassName(router, "/admin/post")} >
+                        <PostsIcon/>
+                        <Link href="#">New Post</Link>
+                    </div>
+                    <div className={getNavItemClassName(router, "")} >
+                        <WorldIcon/>
+                        <Link href="https://vercel.com/malikpiara/moonwith/analytics/audience">Analytics</Link>
                     </div>
 				</div>
 			</div>
